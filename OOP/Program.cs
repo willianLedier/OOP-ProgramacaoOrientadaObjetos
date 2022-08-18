b62cce2a0b943feffb8c800194642d3bdbc49858
Willian Ledier <willian.almeida@socialbank.com.br>
2022-08-18 18:23:06 -0300

    add project

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -2,11 +2,36 @@
 
 namespace OOP
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Escolha a operação:");
+            Console.WriteLine("");
+            Console.WriteLine("1 - Encapsulamento");
+            Console.WriteLine("2 - Heranca Composicao 1");
+            Console.WriteLine("3 - Heranca Composicao 2");
+            Console.WriteLine("4 - Interface Implementacao");
+
+            var opcao = Console.ReadKey();
+
+            switch (opcao.KeyChar)
+            {
+                case '1':
+                    new AutomacaoCafe().ServirCafe();
+                    break;
+                case '2':
+                    new TestesHerancaComposicao();
+                    break;
+                case '3':
+                    new TestesHerancaComposicao2();
+                    break;
+                case '4':
+                    new TesteInterfaceImplementacao();
+                    break;
+            }
+
+            Main();
         }
     }
 }
